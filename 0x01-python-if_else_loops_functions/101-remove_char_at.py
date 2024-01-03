6e0bce6686ac70a431af00a7f67b8ac60b9d8d46
Peter Omollo <omollpeter@gmail.com>
2024-01-03 21:59:43 +0300

    Fix 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -3,10 +3,3 @@ def remove_char_at(str, n):
     if len(str) >= n or len(str) * -1 < n:
         return (str)
     return str.replace(str[n], '')
-
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
