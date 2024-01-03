288a3b90b7d0ac3d5b7e80fff894393f2f857c72
Peter Omollo <omollpeter@gmail.com>
2024-01-03 04:15:04 +0300

    Create a python script that prints all possible different comb. of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i == j:
+            continue
+        if i > j:
+            continue
+        if i == 8 and j == 9:
+            print("{0}{1}".format(i, j))
+        else:
+            print("{0}{1}, ".format(i, j), end="")
