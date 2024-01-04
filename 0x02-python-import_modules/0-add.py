b42ea16300d4bd11f07a56a4bad860120e562ade
Peter Omollo <omollpeter@gmail.com>
2024-01-04 08:01:49 +0300

    Rewrite 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,18 +2,7 @@
 from add_0 import add
 
 
-def use_add(a, b):
-    """Use add function - Uses add function from a module
-
-    Args:
-        a: First integer
-        b: Second integer
-
-    Returns:
-        return value - Nothing
-    """
-    print("{0} + {1} = {2}".format(a, b, add(a, b)))
-
-
+a = 1
+b = 2
 if __name__ == "__main__":
-    use_add(1, 2)
+    print("{0} + {1} = {2}".format(a, b, add(a, b)))
