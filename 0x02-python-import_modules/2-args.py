9c53838c841c1b23104f62ff441f874e00aa8065
Peter Omollo <omollpeter@gmail.com>
2024-01-04 08:58:34 +0300

    Rewrite 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,14 +4,12 @@ import sys
 number = len(sys.argv) - 1
 args = sys.argv
 if number == 0:
-    print(f"{number} argument.")
+    print(f"{number} arguments.")
 elif number == 1:
     print(f"{number} argument:")
 else:
     print(f"{number} arguments:")
 
 if number > 0:
-    for index, item in enumerate(args):
-        if index == 0:
-            continue
-        print("{0}: {1}".format(index, item))
+    for i in range(1, number + 1):
+        print("{0}: {1}".format(i, args[i]))
