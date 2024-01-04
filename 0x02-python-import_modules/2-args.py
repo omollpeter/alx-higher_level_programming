367139ca486e78f6fd922e662a3b0b55c75d5ff7
Peter Omollo <omollpeter@gmail.com>
2024-01-04 09:02:18 +0300

    Fix 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,14 @@ import sys
 
 number = len(sys.argv) - 1
 args = sys.argv
-if number == 0:
-    print(f"{number} arguments.")
-elif number == 1:
-    print(f"{number} argument:")
-else:
-    print(f"{number} arguments:")
+if __name__ == "__main__":
+    if number == 0:
+        print(f"{number} arguments.")
+    elif number == 1:
+        print(f"{number} argument:")
+    else:
+        print(f"{number} arguments:")
 
-if number > 0:
-    for i in range(1, number + 1):
-        print("{0}: {1}".format(i, args[i]))
+    if number > 0:
+        for i in range(1, number + 1):
+            print("{0}: {1}".format(i, args[i]))
