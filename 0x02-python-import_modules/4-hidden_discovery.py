1626155350d0828721c3a675335b7dbb8a10f3b8
Peter Omollo <omollpeter@gmail.com>
2024-01-04 22:18:20 +0300

    Program that prints all defined names in .pyc file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,8 +3,8 @@ import importlib
 
 
 if __name__ == "__main__":
-    module = importlib.import_module("__pycache__/calculator_1")
-    names = dir(module).sort
+    module = importlib.import_module("hidden_4")
+    names = dir(module).sort()
 
     for name in names:
         if (name[0] == '_'):
