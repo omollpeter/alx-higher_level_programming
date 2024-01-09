003f31fa90eb368316a0480a350ccb102a491ac9
Peter Omollo <omollpeter@gmail.com>
2024-01-09 15:17:00 +0300

    Fix 3-print_reversed_list_integer.py and 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -15,6 +15,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tuple_a[0] + tuple_b[0], tuple_b[1]
     elif len_a == 1 and len_b == 0:
         return tuple_a[0], 0
+    elif len_a == 1 and len_b == 1:
+        return tuple_a[0] + tuple_b[0], 0
     elif len_a == 0 and len_b == 1:
         return tuple_b[0], 0
     else:
