2efe6190d90eb59c210a7e452bcabbe2ff755fb7
Peter Omollo <omollpeter@gmail.com>
2024-01-29 17:31:08 +0300

    Write a function that prints a string

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains the following function:
+    say_my_name - Prints a string containing first and
+                    last name
+"""
+
+def say_my_name(first_name, last_name=""):
+    """
+    Prints a string containing first_name and last_name
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
