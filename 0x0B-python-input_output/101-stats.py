08af59638b64addee01bb88d612cbdffe4327186
Peter Omollo <omollpeter@gmail.com>
2024-02-07 10:26:18 +0300

    Fix 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -81,3 +81,32 @@ except KeyboardInterrupt:
     if total_500:
         print(f"500: {total_500}")
     raise
+
+if i % 10:
+    total_200 = stat_codes.count(200)
+    total_301 = stat_codes.count(301)
+    total_400 = stat_codes.count(400)
+    total_401 = stat_codes.count(401)
+    total_403 = stat_codes.count(403)
+    total_404 = stat_codes.count(404)
+    total_405 = stat_codes.count(405)
+    total_500 = stat_codes.count(500)
+
+    if file_size:
+        print(f"File size: {file_size}")
+    if total_200:
+        print(f"200: {total_200}")
+    if total_301:
+        print(f"301: {total_301}")
+    if total_400:
+        print(f"400: {total_400}")
+    if total_401:
+        print(f"401: {total_401}")
+    if total_403:
+        print(f"403: {total_403}")
+    if total_404:
+        print(f"404: {total_404}")
+    if total_405:
+        print(f"405: {total_405}")
+    if total_500:
+        print(f"500: {total_500}")
