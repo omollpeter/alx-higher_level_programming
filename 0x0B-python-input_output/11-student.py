0500e77ab4b1d8e116473acb84c7dc16af35220a
Peter Omollo <omollpeter@gmail.com>
2024-02-07 04:28:28 +0300

    Write a class Student that defines a student by: (based on 10-student.py)

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module contains class definition for Student
+
+"""
+
+
+class Student:
+    """
+    Defines Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes instance attributes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves dict representation of Student instance"""
+        if attrs is None:
+            return self.__dict__
+        else:
+            dictionary = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    dictionary[key] = value
+            return dictionary
+
+    def reload_from_json(self, json):
+        """Replaces all the attributes of Student instance"""
+        self.first_name = json['first_name']
+        self.last_name = json['last_name']
+        self.age = json['age']
