4af050331ad101de2b5a63fa7bcb8ba1c87f03b1
Peter Omollo <omollpeter@gmail.com>
2024-02-09 07:34:54 +0300

    Write Base class and its tests

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+This module contains definition for Base class
+
+"""
+
+
+class Base:
+    """
+    Defines Base class
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        Initializes instance attributes
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
