beb66fc7aeb9fc981dde80756a3764429fb1d36d
Peter Omollo <omollpeter@gmail.com>
2024-02-09 11:11:13 +0300

    Validate setter methods and write their tests

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,18 +29,18 @@ class Rectangle(Base):
 
         if type(width) is not int:
             raise TypeError("width must be an integer")
-        if type(height) is not int:
-            raise TypeError("height must be an integer")
-        if type(x) is not int:
-            raise TypeError("x must be an integer")
-        if type(y) is not int:
-            raise TypeError("y must be an integer")
         if width <= 0:
             raise ValueError("width must be > 0")
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
         if height <= 0:
             raise ValueError("height must be > 0")
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
         if x < 0:
             raise ValueError("x must be >= 0")
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__width = width
@@ -54,8 +54,12 @@ class Rectangle(Base):
         return self.__width
 
     @width.setter
-    def width(self, width):
-        self.__width = width
+    def width(self, wid):
+        if type(wid) is not int:
+            raise TypeError("width must be an integer")
+        if wid <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = wid
 
     @property
     def height(self):
@@ -63,7 +67,11 @@ class Rectangle(Base):
         return self.__height
 
     @height.setter
-    def width(self, height):
+    def height(self, height):
+        if type(height) is not int:
+            raise TypeError("height must be an integer")
+        if height <= 0:
+            raise ValueError("height must be > 0")
         self.__height = height
 
     @property
@@ -73,6 +81,10 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, x):
+        if type(x) is not int:
+            raise TypeError("x must be an integer")
+        if x < 0:
+            raise ValueError("x must be >= 0")
         self.__x = x
 
     @property
@@ -82,4 +94,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, y):
+        if type(y) is not int:
+            raise TypeError("y must be an integer")
+        if y < 0:
+            raise ValueError("y must be >= 0")
         self.__y = y
--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,7 +2,7 @@ import unittest
 from models.rectangle import Rectangle
 
 
-class TestRectangle(unittest.TestCase):
+class TestRectangleId(unittest.TestCase):
 
     def setUp(self):
         self.r1 = Rectangle(10, 2)
@@ -16,3 +16,84 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(self.r1.id, 4)
         self.assertEqual(self.r2.id, 5)
         self.assertEqual(self.r3.id, 12)
+
+
+class TestRectangleObj(unittest.TestCase):
+
+    def setUp(self):
+        self.r1 = Rectangle(10, 2)
+        self.r2 = Rectangle(2, 10)
+        self.r3 = Rectangle(5, 4, 2, 2, 12)
+
+    def tearDown(self):
+        pass
+
+    def test_width(self):
+        self.assertEqual(self.r1.width, 10)
+        self.assertEqual(self.r2.width, 2)
+        self.assertEqual(self.r3.width, 5)
+        self.r1.width = 15
+        self.assertEqual(self.r1.width, 15)
+        self.assertRaises(TypeError, Rectangle, "5", 4)
+        rect = Rectangle(3, 3)
+        self.assertRaises(TypeError, setattr, rect, "width", "4")
+        self.assertRaises(TypeError, setattr, rect, "width", [])
+        self.assertRaises(TypeError, setattr, rect, "width", None)
+        self.assertRaises(TypeError, setattr, rect, "width", {})
+        self.assertRaises(TypeError, setattr, rect, "width", {1, 2})
+        self.assertRaises(TypeError, setattr, rect, "width", 3.15)
+        self.assertRaises(TypeError, setattr, rect, "width", (1, ))
+        self.assertRaises(ValueError, setattr, rect, "width", 0)
+        self.assertRaises(ValueError, setattr, rect, "width", -5)
+
+    def test_height(self):
+        self.assertEqual(self.r1.height, 2)
+        self.assertEqual(self.r2.height, 10)
+        self.assertEqual(self.r3.height, 4)
+        self.r1.height = 7
+        self.assertEqual(self.r1.height, 7)
+        self.assertRaises(TypeError, Rectangle, 5, "4")
+        rect = Rectangle(5, 5)
+        self.assertRaises(TypeError, setattr, rect, "height", "4")
+        self.assertRaises(TypeError, setattr, rect, "height", [])
+        self.assertRaises(TypeError, setattr, rect, "height", None)
+        self.assertRaises(TypeError, setattr, rect, "height", {})
+        self.assertRaises(TypeError, setattr, rect, "height", {1, 2})
+        self.assertRaises(TypeError, setattr, rect, "height", 3.15)
+        self.assertRaises(TypeError, setattr, rect, "height", (1, ))
+        self.assertRaises(ValueError, setattr, rect, "height", 0)
+        self.assertRaises(ValueError, setattr, rect, "height", -5)
+
+    def test_x_coordinate(self):
+        self.assertEqual(self.r1.x, 0)
+        self.assertEqual(self.r2.x, 0)
+        self.assertEqual(self.r3.x, 2)
+        self.r1.x = 15
+        self.assertEqual(self.r1.x, 15)
+        self.assertRaises(TypeError, Rectangle, 5, 4, "2", 2, 151)
+        rect = Rectangle(3, 3)
+        self.assertRaises(TypeError, setattr, rect, "x", "4")
+        self.assertRaises(TypeError, setattr, rect, "x", [])
+        self.assertRaises(TypeError, setattr, rect, "x", None)
+        self.assertRaises(TypeError, setattr, rect, "x", {})
+        self.assertRaises(TypeError, setattr, rect, "x", {1, 2})
+        self.assertRaises(TypeError, setattr, rect, "x", 3.15)
+        self.assertRaises(TypeError, setattr, rect, "x", (1, ))
+        self.assertRaises(ValueError, setattr, rect, "x", -5)
+
+    def test_y_coordinate(self):
+        self.assertEqual(self.r1.y, 0)
+        self.assertEqual(self.r2.y, 0)
+        self.assertEqual(self.r3.y, 2)
+        self.r1.y = 15
+        self.assertEqual(self.r1.y, 15)
+        self.assertRaises(TypeError, Rectangle, 5, 4, 2, "2", 152)
+        rect = Rectangle(3, 3)
+        self.assertRaises(TypeError, setattr, rect, "y", "4")
+        self.assertRaises(TypeError, setattr, rect, "y", [])
+        self.assertRaises(TypeError, setattr, rect, "y", None)
+        self.assertRaises(TypeError, setattr, rect, "y", {})
+        self.assertRaises(TypeError, setattr, rect, "y", {1, 2})
+        self.assertRaises(TypeError, setattr, rect, "y", 3.15)
+        self.assertRaises(TypeError, setattr, rect, "y", (1, ))
+        self.assertRaises(ValueError, setattr, rect, "y", -5)
