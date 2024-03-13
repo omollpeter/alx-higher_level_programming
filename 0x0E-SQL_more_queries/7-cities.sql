94b10963d560ffa7c85425ce114bab5ce1e014db
Peter Omollo <omollpeter@gmail.com>
2024-03-13 19:06:04 +0300

    Fix 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS cities(
     id INT PRIMARY KEY AUTO_INCREMENT,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+    FOREIGN KEY (state_id) REFERENCES states(id),
+    UNIQUE(id)
 );
