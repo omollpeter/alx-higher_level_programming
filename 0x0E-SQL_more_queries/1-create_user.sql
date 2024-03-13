6a52b50715340441e19ee58c727a41bc893fe5f9
Peter Omollo <omollpeter@gmail.com>
2024-03-13 12:50:35 +0300

    @a34639941Z

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates a user and grants it all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
