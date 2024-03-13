146d92ffd54dc279a0f7b4fc7a4f60b0fdce53b0
Peter Omollo <omollpeter@gmail.com>
2024-03-13 18:45:25 +0300

    Write a sql script that creates a table with foreign key

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,9 @@
+-- Creates a database and a table with foreign key
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+USE hbtn_0d_2;
+CREATE TABLE IF NOT EXISTS cities(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
