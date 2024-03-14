d7e82cd6eff79e601ac9d47d5feb13d73300a98b
Peter Omollo <omollpeter@gmail.com>
2024-03-14 04:57:22 +0300

    Fix 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,4 +4,4 @@ FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_genres.name ASC;
+ORDER BY title ASC;
