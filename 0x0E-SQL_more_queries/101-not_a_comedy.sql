6d6528728db6c4b69bffdc18173d9dcb1f351c4d
Peter Omollo <omollpeter@gmail.com>
2024-03-14 05:36:53 +0300

    Write a sql script that lists all non comedy shows

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- Lists all non-comedy shows
+SELECT tv_shows.title FROM tv_shows WHERE tv_shows.title NOT IN (
+    SELECT tv_shows.title AS title
+    FROM tv_genres
+    JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title ASC;
