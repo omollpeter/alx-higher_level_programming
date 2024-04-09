4eb3f9fb37071d4a415a1baa1a438dfa2f0f8fff
Peter Omollo <omollpeter@gmail.com>
2024-04-09 18:13:37 +0300

    Fix 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 class Rectangle {
-  width;
-  height;
-
   constructor (w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      this.width = null;
-      this.height = null;
+      return;
     } else {
       this.width = w;
       this.height = h;
