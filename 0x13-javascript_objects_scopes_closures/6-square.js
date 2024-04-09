200a289cbf0a6b7b4adba62e736125ba01b3f07b
Peter Omollo <omollpeter@gmail.com>
2024-04-09 19:13:42 +0300

    Fix 6-squarere.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -33,11 +33,11 @@ class Square extends Rectangle {
 
   charPrint (c) {
     if (c === undefined) {
-      this.print();
-    } else {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.height));
-      }
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
     }
   }
 }
