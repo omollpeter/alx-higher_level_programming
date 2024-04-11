56f5aa77e02d211e362e306a6c15fe3af686da7a
Peter Omollo <omollpeter@gmail.com>
2024-04-11 07:34:43 +0300

    Write a script that lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module is a script that lists all states from the database
+hbtn_0e_0_usa
+It uses MySQLdb module/interface to connect to the database and
+execute the queries
+The script takes three arguments - mysql username, mysql password
+and database name
+"""
+
+
+import MySQLdb
+import sys
+
+
+username = sys.argv[1]
+password = sys.argv[2]
+database = sys.argv[3]
+table = "states"
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd=password,
+    db=database,
+)
+
+cursor = db.cursor()
+cursor.execute(f"SELECT * FROM {table}")
+data = cursor.fetchall()
+
+for row in data:
+    print(row)
