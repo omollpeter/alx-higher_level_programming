25b9c73381203c268c518401d86d0e711150a230
Peter Omollo <omollpeter@gmail.com>
2024-04-11 16:37:02 +0300

    Write a script that takes in the name of a state as an argument and lists all cities of that state, using the database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -31,8 +31,8 @@ if __name__ == "__main__":
     cursor.execute(
         "SELECT cities.id, cities.name, states.name FROM cities" +
         " LEFT JOIN states" +
-        " ON cities.state_id = states.id"
-        " ORDER BY id ASC"
+        " ON cities.state_id = states.id" +
+        " ORDER BY cities.id ASC"
     )
     data = cursor.fetchall()
 
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This module is a script takes in the name of a state as an argument and
+lists all cities of that state, using the database hbtn_0e_4_usa
+
+It uses MySQLdb module/interface to connect to the database and
+execute the queries
+The script takes 4 arguments - mysql username, mysql password,
+database name and search state arg
+"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    srch_name = sys.argv[4]
+    table = "states"
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT cities.name FROM cities" +
+        " JOIN states ON cities.state_id = states.id" +
+        " WHERE states.name = %s", (srch_name,)
+    )
+    data = cursor.fetchall()
+
+    cities = []
+    for row in data:
+        cities.append(row[0])
+
+    for i in range(len(cities)):
+        print(cities[i], end="")
+        if i == len(cities) - 1:
+            print()
+            continue
+        print(", ", end='')
+
+    cursor.close()
+    db.close()
