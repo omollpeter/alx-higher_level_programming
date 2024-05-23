137eddfc34223586b090678f1a38411796742591
Peter Omollo <omollpeter@gmail.com>
2024-05-23 06:00:37 +0300

    Fix 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,26 +2,26 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-function fetchData(url) {
-    return new Promise((resolve, reject) => {
-        request(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-            }
-            resolve(JSON.parse(body).name);
-        });
+function fetchData (url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(JSON.parse(body).name);
     });
+  });
 }
 
-async function fetchMultiple(urls) {
-    if (urls.length === 0) {
-        return [];
-    }
+async function fetchMultiple (urls) {
+  if (urls.length === 0) {
+    return [];
+  }
 
-    const url = urls.shift();
-    const data = await fetchData(url);
-    const remData = await fetchMultiple(urls);
-    return [data, ...remData];
+  const url = urls.shift();
+  const data = await fetchData(url);
+  const remData = await fetchMultiple(urls);
+  return [data, ...remData];
 }
 
 request(url, (error, response, body) => {
@@ -31,13 +31,13 @@ request(url, (error, response, body) => {
   }
 
   const characters = JSON.parse(body).characters;
-    fetchMultiple(characters)
-        .then(results => {
-            for (for const res of results) {
-                console.log(res);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        })
+  fetchMultiple(characters)
+    .then(results => {
+      for (const res of results) {
+        console.log(res);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
